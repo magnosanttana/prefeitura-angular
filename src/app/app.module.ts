4388fa77ddc02a528cc8033134b873eb1573ad32
Magno Santtana <Magno Santtana>
2018-03-09 17:30:27 -0300

    Adiciona layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AsideComponent } from './aside/aside.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AsideComponent
   ],
   imports: [
     BrowserModule
